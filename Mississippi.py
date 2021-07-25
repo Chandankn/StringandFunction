1eae55e8c428c7f5817efed2556ce6c494ccb999
Chandankn <87237018+Chandankn@users.noreply.github.com>
2021-07-25 10:19:20 -0700

    Created by Chandan String and function

---
+++ Mississippi.py
@@ -0,0 +1,14 @@
+#include<stdio.h>
+S = input("Please enter a string")
+def most_frequent(string):
+    d = dict()
+    for keyin string:
+       if key not in d:
+       d[key] = 1
+     else:
+       d[key] += 1
+    return d
+    
+print(most_frequent(S))    
+    
+       
